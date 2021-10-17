886c5c2a6d8f5aa2dfe1784e96fc747b81405f6c
Emilia <m.emilia.guadalupe@gmail.com>
2021-10-17 19:18:54 -0300

    Starting with the components..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+
+import { getTokenFromUrl } from './Helpers/spotifyAuth';
 
 //Components
 import Login from './Components/Login/Login'
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+    }
+
+  }, [])
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (<h1>Soy el repro de música</h1>) : <Login />
+      }
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './loginStyles.css'
 
+import { accessUrl } from '../../Helpers/spotifyAuth';
+
 export default function Login() {
     return(
         <div className="login-container">
-
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo" />
+            <a href={accessUrl} className="login-container__button">LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
