3a7f5260fb8d42ca1b04e25188e5bca17543a5e5
Emilia <m.emilia.guadalupe@gmail.com>
2021-10-16 21:32:46 -0300

    Adding the component to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+//Components
+import Login from './Components/Login/Login'
+
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
