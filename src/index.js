790aafed267a2054a9774229246b242f953e4a7d
Emilia <m.emilia.guadalupe@gmail.com>
2021-10-23 19:22:30 -0300

    Adding a Context Provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { DataContextProvider } from './Context/DataProvider';
+import { initialState } from './reducer';
+import reducer from './reducer';
+
+
+
 ReactDOM.render(
   <React.StrictMode>
+    <DataContextProvider initialState={initialState} reducer={reducer}>
     <App />
+    </DataContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
